e571fec486adb7bdb76253cf108a41e644b6b6b2
rlee21 <rlee21@comcast.net>
2021-12-01 10:04:29 -0800

    add Standup and Tech Debt roles and update html class names

--- app/models/roles_generator.rb
+++ app/models/roles_generator.rb
@@ -1,8 +1,8 @@
 class RolesGenerator
 
-  ROLES = roles = [:retro, :planning, :roles_rotation].freeze
+  ROLES = ["Planning", "Roles Rotation", "Retro", "Standup", "Tech Debt"].freeze
 
-  def self.call(team: "consumer")
+  def self.call(team: "Consumer")
     members = User.where(team: team.titlecase, active: true).pluck(:name)
     members.shuffle.zip(ROLES.shuffle).reject { |member_role| member_role.last.nil? }
   end
